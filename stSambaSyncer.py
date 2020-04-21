c21094d2d0aa3090170f34b5767d49ed6c47739f
summerffly <afeism@foxmail.com>
2020-04-21 23:40:36 +0800

    UPD <stSambaSyncer>
    - 拆分文件夹/文件比对函数

--- stSambaSyncer.py
+++ stSambaSyncer.py
@@ -17,7 +17,7 @@ def MakeDecision():
     print("")
     decision = input(">>> 确定同步？(Y/N): ")
     if decision == "Y":
-        return decision
+        pass
     elif decision == "N":
         print("")
         print("------------------------------")
@@ -27,7 +27,7 @@ def MakeDecision():
     else:
         print("")
         print("------------------------------")
-        print(">>> Select Error !!! <<<")
+        print(">>> 选择错误 ! <<<")
         print("------------------------------")
         print("")
     return decision
@@ -38,37 +38,39 @@ def SyncSambaNetdisk(src_dir_path, dst_dir_path):
     dir_list = list()
     file_list = list()
     DirTraverser(src_dir_path, dir_list, file_list)
+    print("Path >>> " + src_dir_path)
 
-    print(">>>>>>>>>><<<<<<<<<<<")
-    print(">>> Sync Dir List <<<")
-    print(">>>>>>>>>><<<<<<<<<<<")
-    for dir_name in dir_list:
-        dir_len = len(src_dir_path)
-        dst_subdir_path = dst_dir_path + dir_name[dir_len:]
-        if os.path.exists(dst_subdir_path) == False:
-            print(dst_subdir_path)
-    print("")
-    print(">>>>>>>>>>><<<<<<<<<<<")
-    print(">>> Sync File List <<<")
-    print(">>>>>>>>>>><<<<<<<<<<<")
-    for file_name in file_list:
-        dir_len = len(src_dir_path)
-        src_parent_path = os.path.dirname(file_name)
-        dst_parent_path = dst_dir_path + src_parent_path[dir_len:]
-        src_parent_len = len(src_parent_path)+1
-        ffile_name = file_name[src_parent_len:]
-        if ffile_name[0] != ".":
-            fffile_name = dst_parent_path + '\\' + ffile_name
-            if os.path.exists(fffile_name) == False:
-                print(fffile_name)
+    dst_dir_list = list()
+    DirComparer(src_dir_path, dst_dir_path, dir_list, dst_dir_list)
+    dst_file_list = list()
+    FileComparer(src_dir_path, dst_dir_path, file_list, dst_file_list)
+
+    if len(dst_dir_list) == 0:
+        if len(dst_file_list) == 0:
+            print(">>> Dir List Synced : )")
+            print(">>> File List Synced : )")
+            print("")
+            return
+
+    if len(dst_dir_list) != 0:
+        print(">>>>>>>>>><<<<<<<<<<<")
+        print(">>> Sync Dir List <<<")
+        print(">>>>>>>>>><<<<<<<<<<<")
+        for dir_name in dst_dir_list:
+            print(dir_name)
+
+    if len(dst_file_list) != 0:
+        print(">>>>>>>>>><<<<<<<<<<<")
+        print(">>> Sync File List <<<")
+        print(">>>>>>>>>><<<<<<<<<<<")
+        for file_name in dst_file_list:
+            print(file_name)
 
     if MakeDecision() == "Y":
-        for dir_name in dir_list:
-            dir_len = len(src_dir_path)
-            dst_subdir_path = dst_dir_path + dir_name[dir_len:]
-            if os.path.exists(dst_subdir_path) == False:
-                os.makedirs(dst_subdir_path)
-                print(dst_subdir_path)
+        if os.path.exists(dst_dir_path) == False:
+            os.makedirs(dst_dir_path)
+        for dir_name in dst_dir_list:
+            os.makedirs(dir_name)
         for file_name in file_list:
             #shutil.copyfile(filename, bkfilename)
             #shutil.copy2(filename, dstdirpath)
@@ -82,6 +84,30 @@ def SyncSambaNetdisk(src_dir_path, dst_dir_path):
                 if os.path.exists(fffile_name) == False:
                     shutil.copy2(file_name, dst_parent_path)
                     print(file_name)
+        print("")
+
+
+### 文件夹 比对 ###
+def DirComparer(src_dir_path, dst_dir_path, src_dir_list, dst_dir_list):
+    for src_dir_name in src_dir_list:
+        src_dir_len = len(src_dir_path)
+        dst_subdir_path = dst_dir_path + src_dir_name[src_dir_len:]
+        if os.path.exists(dst_subdir_path) == False:
+            dst_dir_list.append(dst_subdir_path)
+
+
+### 文件 比对 ###
+def FileComparer(src_dir_path, dst_dir_path, src_file_list, dst_file_list):
+    for file_name in src_file_list:
+        src_dir_len = len(src_dir_path)
+        src_parent_path = os.path.dirname(file_name)
+        dst_parent_path = dst_dir_path + src_parent_path[src_dir_len:]
+        src_parent_len = len(src_parent_path)+1
+        ffile_name = file_name[src_parent_len:]
+        if ffile_name[0] != ".":
+            fffile_name = dst_parent_path + '\\' + ffile_name
+            if os.path.exists(fffile_name) == False:
+                dst_file_list.append(fffile_name)
 
 
 ### 文件夹逐层遍历 ###
@@ -115,7 +141,6 @@ if __name__ == "__main__":
     print("------------------------------")
     print("")
 
-    #srcdirpath = "\\192.168.11.1\samba\2020.BOX.V"
     srcdirpath1 = "\\\\192.168.11.1\\samba\\AD.PornTuTu"
     dstdirpath1 = "W:\\Bakup.Samba\\AD.PornTuTu"
     SyncSambaNetdisk(srcdirpath1, dstdirpath1)
@@ -128,9 +153,9 @@ if __name__ == "__main__":
     dstdirpath3 = "W:\\Bakup.Samba\\AD.iMagazine"
     SyncSambaNetdisk(srcdirpath3, dstdirpath3)
 
-    #srcdirpath4 = "\\\\192.168.11.1\\samba\\2020.BOX.V"
-    #dstdirpath4 = "W:\\Bakup.Samba\\2020.BOX.V"
-    #SyncSambaNetdisk(srcdirpath4, dstdirpath4)
+    srcdirpath4 = "\\\\192.168.11.1\\samba\\2020.BOX.V"
+    dstdirpath4 = "W:\\Bakup.Samba\\2020.BOX.V"
+    SyncSambaNetdisk(srcdirpath4, dstdirpath4)
 
     print("")
     print("------------------------------")
